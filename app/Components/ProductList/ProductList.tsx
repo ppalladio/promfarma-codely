f5463571298f02a2072ca1a2d286148aefbee04a
Yorgo Petsas <yorgopecas@gmail.com>
2023-07-10 20:13:32 +0200

    Product List through Fetch Hook

--- app/Components/ProductList/ProductList.tsx
+++ app/Components/ProductList/ProductList.tsx
@@ -1,54 +1,68 @@
-import Card from '@/app/Card';
 import React from 'react';
-// import { products } from '../Constants/products';
-import { products } from '../Api/products.ts';
-// import { category } from '../Api/category.ts';
-import { category, manufacturerName, brandName } from '../Constants/info.ts';
+import useProductList from '../Api/fetchProductData';
 
-interface Price {
-    amount?: number;
-    currency?: string;
-    country?: string;
-}
 const ProductList = () => {
-    return (
-        <div>
-            {products.map((product) => {
-                // const price = product.recommended_prices.find(
-                //     (price) => price.country === 'ES',
-                // ); //@in case price ES does not exist
+  const apiUrl = 'https://graphql.stg.promofarma.com/graphql';
+  const pageSize = 14;
 
-                return (
-                    <div key={product.product_id}>
-                        <Card
-                            id={product.product_id}
-                            name={product?.name ?? ''}
-                            // country={price?.country}
-                            // price={price?.amount}
-                            state={product.product_state}
-                            brandId={product.brand.brand_id}
-                            brandName= {brandName[product.brand.brand_id]}
-                            manufacturerId={
-                                manufacturerName[
-                                    product.manufacturer.manufacturer_id
-                                ]
-                            }
-                            manufacturerName={
-                                product.manufacturer.manufacturer_name
-                            }
-                            favorite={false} // Set your favorite logic here
-                            favoriteImg={''} // Set the favorite image URL if needed
-                            productIndex={''} // Set the product index if needed
-                            // category={
-                            //     category[product.main_category!.category_id]
-                            // } //! category can be null
-                            hasStock={product.has_stock}
-                        />
-                    </div>
-                );
-            })}
+  const products = useProductList(apiUrl, pageSize);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      {products.map((product) => (
+        <div>
+          <h2><b>Product Name:</b> {product.name}</h2>
+          <h2><b>Product ID:</b> {product.product_id}</h2>
+          <h2><b>Product updated_at:</b> {product.updated_at}</h2>
+          <h2><b>Product Has Stock:</b> {product.has_stock}</h2>
+          <h3><b>Product Brand:</b> {product.brand.name}</h3>
+          <h3><b>Product Brand ID:</b> {product.brand.brand_id}</h3>
+          <h3><b>Product State:</b> {product.product_state}</h3>
+          <h3><b>Product Categories:</b></h3>
+            <ul>
+              {Object.entries(product.manufacturer).map(([key, value]) => (
+                <li key={key}>
+                  <strong>{key}: </strong>
+                  {value}
+                </li>
+              ))}
+            </ul>
+          Main Category:
+          {product.main_category ? (
+            <ul>
+              {Object.entries(product.main_category).map(([key, value]) => (
+                <li key={key}>
+                  <strong>{key}: </strong>
+                  {value}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No main category available.</p>
+          )}
+          Recommended Prices:
+          <ul>
+            {product.recommended_prices.map((price, index) => (
+              <li key={index}>
+                <strong>Amount: </strong>
+                {price.amount}
+                <br />
+                <strong>Currency: </strong>
+                {price.currency}
+                <br />
+                <strong>Country: </strong>
+                {price.country}
+              </li>
+            ))}
+          </ul>
+          <br />
+          {/* <h3><b>Product Category:</b> {product.category.name}</h3> */}
+          {/* <h3><b>Product Category:</b> {product.main_category.category_name}</h3> */}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
