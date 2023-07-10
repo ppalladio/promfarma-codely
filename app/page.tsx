05485b975b1f241aa0c911fd3512ebe1dc5b998f
Yorgo Petsas <yorgopecas@gmail.com>
2023-07-10 20:14:42 +0200

    temp version of working files

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,38 @@
+'use client'
 import 'app/App.css';
-// import React from 'react';
+import React from 'react';
 import GeoLocation from './Components/GeoLocation/GeoLocation';
-import ProductList from './Components/ProductList/ProductList';
-import BrandList from './Components/BrandList/BrandList';
-import CategoryList from './Components/CategoryList/CategoryList';
-import ManufacturerList from './Components/ManufacturerList/ManufacturerList';
+import BrandList from './Components/BrandList/newBrandList';
+import CategoryList from './Components/CategoryList/newCategoryList';
+import ProductList from './Components/ProductList/newProductList';
+import ManufacturerList from './Components/ManufacturerList/newManufacturerList';
+import SideBar from './Components/Sidebar/Sidebar';
+import ToggledTags from '../Sidebar/ToggleTags';
 
-// TEMP
-import React, { useEffect, useState } from 'react';
-// import TestList from './Components/TestComponent/testComponent';
-// import CategoryDictionary from './Components/Api/test-api-react';
-// import CategoryDictionaryComponent from './Components/Api/test2'
-// Context TEST
-// import NavBar from "./Components/NavBa/NavBar";
-// import PostsPage from "./Components/PostsPage/PostsPage";
-// import AuthContextProvider from '/Users/yorgopetsas/github/tests/code/app/Contexts/AuthContext.js'; 
-import CategoryDictionary from '/Users/yorgopetsas/github/tests/code/app/Components/Api/CategoryDictionary';
+// LATER
+// import ProductList from './Components/ProductList/ProductList';
+// import ManufacturerList from './Components/ManufacturerList/ManufacturerList';
+// import BrandList from './Components/BrandList/BrandList';
 
 function App() { 
-
   return (
       <>
-        {/* {CategoryDictionary /} */}
-        {/* {/* <NavBar />  */}
-        {/* <PostsPage />  //  user={user} */}
         <div>
-            <GeoLocation /><br />
-        </div>
-        <div>
-            {/* <TestList /><br /> */}
-            {/* <CategoryDictionaryComponent /><br /> */}
+            {/* <GeoLocation /><br /> */}
         </div>
         <div style={{float: 'left',}}>
-            <BrandList /><br />
-            
+            {/* <BrandList /><br /> */}
         </div>
         <div style={{float: 'left',}}>
-            <ManufacturerList /><br />
+            {/* <ManufacturerList /><br /> */}
         </div>
         <div>
-              <CategoryList /><br />
+              {/* <CategoryList /><br /> */}
         </div>
         <div>
-              <ProductList/>
+              {/* <ProductList/> */}
+              <SideBar />
+              
         </div>
       </>
   );
