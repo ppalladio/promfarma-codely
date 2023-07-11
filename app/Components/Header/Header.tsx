19e6537ad352e7e69d434ce52813298c2a36798a
ppalladio <nine.slide@gmail.com>
2023-07-11 20:57:38 +0200

    header styling

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
 import './Header.css';
-
+import Image from 'next/image';
+import DesktopLogo from '../../assets/logo-desktop.svg';
 const NamePage = () => {
-  return (
-    <Link href="/" passHref>
-      <span className="logo-link">
-        <h1 className="namePage">PromoFarma</h1>
-      </span>
-    </Link>
-  );
+    return (
+        <Link href="/" passHref>
+            <span className="logo-link ">
+                <Image
+                    alt="desktop logo"
+                    src={DesktopLogo}
+                    className='w-15'
+                />
+            </span>
+        </Link>
+    );
 };
 
 const Buttons = () => {
-  return (
-    <div className="button-container">
-      <Link href="/favorites" passHref>
-        <span className="headerNav">Favorites</span>
-      </Link>
-      <Link href="/aboutUs" passHref>
-        <span className="headerNav">About us</span>
-      </Link>
-    </div>
-  );
+    return (
+        <div className="button-container">
+            <Link href="/favorites" passHref>
+                <span className="headerNav">Favorites</span>
+            </Link>
+            <Link href="/aboutUs" passHref>
+                <span className="headerNav">About us</span>
+            </Link>
+        </div>
+    );
 };
 
 const Header = () => {
-  return (
-    <header className="header">
-      <NamePage />
-      <Buttons />
-    </header>
-  );
+    return (
+        <header className="header lg:bg-white bg-primary_green text-white lg:text-base">
+            <NamePage />
+            <Buttons />
+        </header>
+    );
 };
 
 export default Header;
