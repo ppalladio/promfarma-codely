fdc4b60052c60f04bcaa988728fb8baf220b85d2
Laura <garch.laura@gmail.com>
2023-07-11 09:37:30 +0200

    Header

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,10 +1,36 @@
-'use client'
+import React from 'react';
+import Link from 'next/link';
+import './Header.css';
 
+const NamePage = () => {
+  return (
+    <Link href="/" passHref>
+      <span className="logo-link">
+        <h1 className="namePage">PromoFarma</h1>
+      </span>
+    </Link>
+  );
+};
 
+const Buttons = () => {
+  return (
+    <div className="button-container">
+      <Link href="/favorites" passHref>
+        <span className="headerNav">Favorites</span>
+      </Link>
+      <Link href="/aboutUs" passHref>
+        <span className="headerNav">About us</span>
+      </Link>
+    </div>
+  );
+};
 
 const Header = () => {
   return (
-<div>header</div>
+    <header className="header">
+      <NamePage />
+      <Buttons />
+    </header>
   );
 };
 
